b223c31f57288d2774b3d74a0c362982d8e21447
rand0451 <96317777+rand0451@users.noreply.github.com>
2024-11-24 08:50:23 +0100

    Update App.jsx

--- front-end/ems-frontend/src/App.jsx
+++ front-end/ems-frontend/src/App.jsx
@@ -6,31 +6,32 @@ import Chat from "./components/Pages/Chat.jsx";
 import Home from "./components/Pages/Home.jsx";
 import Login from "./components/Pages/Login.jsx";
 import Registration from "./components/Pages/Registration.jsx";
+
+
 function App() {
-  
+
   return (
-  <>
-  <BrowserRouter>
+      <>
+        <BrowserRouter>
 
-      <Routes>
-        {/* // http://localhost:3000 */}
-        <Route path='/' element = { <Home/> }> </Route>
+          <Routes>
+            {/* // http://localhost:3000 */}
+            <Route path='/' element = { <Home/> }> </Route>
 
-        {/* // http://localhost:3000/login */}
-        <Route path='/login' element = { <Login/> } > </Route>
+            {/* // http://localhost:3000/login */}
+            <Route path='/login' element = { <Login/> } > </Route>
 
-        {/* // http://localhost:3000/registration */}
-        <Route path='/registration' element = { <Registration/> }> </Route>
+            {/* // http://localhost:3000/registration */}
+            <Route path='/registration' element = { <Registration/> }> </Route>
 
-        {/* // http://localhost:3000/chat */}
-        <Route path='/chat' element = { <Chat/> }> </Route>
-      </Routes>
+            {/* // http://localhost:3000/chat */}
+            <Route path='/chat' element = { <Chat/> }> </Route>
+          </Routes>
 
-
-
-  </BrowserRouter>
-  </>
+        </BrowserRouter>
+      </>
   )
 }
 
 export default App
+
